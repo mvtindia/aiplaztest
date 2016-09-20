11a0c03b0fa5807bc3cf339acb41226e90f6c034
mvtindia <mvt.indiadev@outlook.com>
2016-09-20 21:17:18 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,22 +1,12 @@
 <?php
 error_reporting(0);
-
-
-<<<<<<< HEAD
-//$connect=mysqli_connect("us-cdbr-azure-east-c.cloudapp.net","bcdc8eec7d9c69","70936a9a","TurtleRiver_testDB");
-//$connect=mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net","b660bd5f89b637","ed5c4a00","Yamuna");
-$connect=mysqli_connect("localhost:8888","","","2finda_local");
-=======
 $connect=mysqli_connect("us-cdbr-azure-east-c.cloudapp.net","bcdc8eec7d9c69","70936a9a","TurtleRiver_testDB");
-//$connect=mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net","b660bd5f89b637","ed5c4a00","Yamuna");
->>>>>>> origin/test
-
-
+//$connect=mysqli_connect("us-cdbr-azure-northcentral-b.cloudapp.net","b5ed6b723b4e0c","7bf07005","chicago_river");
+//"ap-cdbr-azure-east-c.cloudapp.net","b660bd5f89b637","ed5c4a00","Yamuna"
 if($connect){
 echo "";
 }
 else{
 	echo "Error in Establishing a Connection with database!";
 }
-
 ?>
