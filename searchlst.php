424764227ca4c45907329e2272be562631878891
Andy Guschwan <aguschwan@gmail.com>
2016-12-06 14:02:51 -0600

    book a space

--- searchlst.php
+++ searchlst.php
@@ -131,42 +131,43 @@ $guests = 1;
 
 <body>
 <div class=""><!--container-fluid start-->
-<div class="">
+  <div class="">
 
 
 <!--==============menu header=========================-->
-<div class="menu-had2" style="    position: fixed;
-    z-index: 20;
-    top: 0px;
-    width: 100%;">
-<?php include 'lib/header.php';?>
-</div><!--menu-had close-->
+   <div class="menu-had2" style="    position: fixed;
+      z-index: 20;
+      top: 0px;
+      width: 100%;">
+      <?php include 'lib/header.php';?>
+    </div><!--menu-had close-->
 <!--==============menu header close=========================-->
 
 
-<div class="">
-<div class="main-center-data" style="margin-top: 129px;">
-<div class="search-lst-data">
-<div class="col-md-5 pd-l-0 hidden-sm hidden-xs">
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAHOSxxua5IIFTA1-WiZbenpWIj0yv9hU8"></script>
+    <div class="">
+      <div class="main-center-data" style="margin-top: 129px;">
+        <div class="search-lst-data">
+          <div class="col-md-5 pd-l-0 hidden-sm hidden-xs">
+            <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAHOSxxua5IIFTA1-WiZbenpWIj0yv9hU8"></script>
 
-<div id="map_canvas"></div>
-</div>
-</div>
+            <div id="map_canvas"></div>
+          </div>
+        </div>
 
-<div class="col-md-7 col-sm-12 col-xs-12">
-<h1 class="search-had"><?php echo $place_loc; ?></h1>
-<div class="moon-divider2 small"></div>
+        <!--<div class="col-md-7 col-sm-12 col-xs-12" style="border: solid 1px;">-->
+        <div class="col-md-7 col-sm-12 col-xs-12" style="border: solid 1px;">
+          <h1 class="search-had"><?php echo $place_loc; ?></h1>
+<!--<div class="moon-divider2 small"></div>-->
 
-<form method="post" action="searchlst.php">
-
-<div class="row">
-<div class="col-md-2">
-<p>Budget</p>
-</div>
-<div class="col-md-4" style="padding-left:0px">
-<input type="hidden" name="my-address" id="my-address" value="<?php echo $place_loc;?>">
-<input type="hidden" name="mylat" id="mylat" value="<?php if($mylat=='')
+          <form method="post" action="searchlst.php">
+          <div class="had-frm" >Search</div>
+          <div class="row">
+            <div class="col-md-2">
+              <p>Budget</p>
+            </div>
+            <div class="col-md-4" style="padding-left:0px">
+              <input type="hidden" name="my-address" id="my-address" value="<?php echo $place_loc;?>">
+              <input type="hidden" name="mylat" id="mylat" value="<?php if($mylat=='')
 {
  echo 17.3700;
 $mylng = 78.4800;
@@ -177,7 +178,7 @@ else
   echo $mylat; 
   }
   ?>">
-<input type="hidden" name="mylng" id="mylng" value="<?php  if($mylng=='')
+              <input type="hidden" name="mylng" id="mylng" value="<?php  if($mylng=='')
 {
 echo  78.4800;
 }
@@ -187,19 +188,19 @@ else
   echo $mylng; 
   } ?>">
 
-  <input style="width:50%;float:left" type="number" id="input-select" name="minbud" class="form-control">
-  <input style="width:50%;" class="form-control" type="number" min="1" max="50000" name="maxbud" step="1" id="input-number">
-</div>
-<div class="col-md-6">
-<div id="html5" class="noUi-target noUi-ltr noUi-horizontal noUi-background"></div>
-</div>
-</div>
+              <input style="width:50%;float:left" type="number" id="input-select" name="minbud" class="form-control">
+              <input style="width:50%;" class="form-control" type="number" min="1" max="50000" name="maxbud" step="1" id="input-number">
+            </div>
+            <div class="col-md-6">
+              <div id="html5" class="noUi-target noUi-ltr noUi-horizontal noUi-background"></div>
+              </div>
+            </div>
 
-<div class="row mg-top20">
-<div class="col-md-2">
-<p>Date</p>
-</div>
-<div class="col-md-4" style="padding-left:0px">
+            <div class="row mg-top20">
+              <div class="col-md-2">
+                <p>Date</p>
+              </div>
+              <div class="col-md-4" style="padding-left:0px">
 <?php 
 if(!empty($_REQUEST['daterange']))
 {
@@ -214,34 +215,34 @@ $final1 = date('m/d/Y');
 }
 
  ?>
-<input  style="width:50%;float:left" type="text" id="datepicker" value="<?php 
+                <input  style="width:50%;float:left" type="text" id="datepicker" value="<?php 
 echo $final; ?>" name="chin" class="form-control" placeholder="From">
-<input style="width:50%;" type="text" id="datepicker1" value="<?php 
+                <input style="width:50%;" type="text" id="datepicker1" value="<?php 
 echo $final1; ?>" name="chout" class="form-control" placeholder="To">
-</div>
+              </div>
 
-<div class="col-md-2">
-<p>Location</p>
-</div>
-<div class="col-md-4">
-<input type="text" value="<?php echo $place_loc; ?>" id="autocomplete" name="place_loc" onFocus="geolocate();" onchange="geolocate()" class="form-control" >
- <input type="hidden" name="my-lat" id="my-lat" value="">
- <input type="hidden" name="my-lng" id="my-lng" value="">
-</div>
-</div>
+              <div class="col-md-2">
+                <p>Location</p>
+              </div>
+              <div class="col-md-4">
+                <input type="text" value="<?php echo $place_loc; ?>" id="autocomplete" name="place_loc" onFocus="geolocate();" onchange="geolocate()" class="form-control" >
+                <input type="hidden" name="my-lat" id="my-lat" value="">
+                <input type="hidden" name="my-lng" id="my-lng" value="">
+              </div>
+            </div>
 
-<div class="row mg-top20">
-<div class="col-md-2">
-<p>Guests</p>
-</div>
-<div class="col-md-4" style="padding-left:0px">
-<input type="text" value="<?php echo $guests; ?> Guests" name="guests" class="form-control" >
-</div>
+            <div class="row mg-top20">
+              <div class="col-md-2">
+                <p>Guests</p>
+              </div>
+              <div class="col-md-4" style="padding-left:0px">
+                <input type="text" value="<?php echo $guests; ?> Guests" name="guests" class="form-control" >
+              </div>
 
-<div class="col-md-2">
-<p>Events</p>
-</div>
-<div class="col-md-4">
+              <div class="col-md-2">
+                <p>Events</p>
+              </div>
+              <div class="col-md-4">
  <!-- <input type="text" class="form-control bord" placeholder="Event"> -->
                           <select id="select1" class="form-control bord" multiple="multiple" name="events[]" >
                                <!--  <option value="" hidden>Select Uses</option> -->
@@ -263,9 +264,8 @@ echo $final1; ?>" name="chout" class="form-control" placeholder="To">
                               
                           <?php }//while ?>
                           </select> 
-</div>       
-
-</div>
+              </div>
+            </div>
 <!-- <div class="row mg-top20">
 <div class="col-md-2">
 <p>Sort by</p>
@@ -288,18 +288,18 @@ echo $final1; ?>" name="chout" class="form-control" placeholder="To">
       </div>
 </div>
 </div> -->
-<button class="btn-3 mg-top20 mg-bottom20" type="submit">Search</button>
-</form>
-<div class="row">
+            <button class="btn-3 mg-top20 mg-bottom20" type="submit">Search</button>
+          </form>
+          <div class="row">
 <?php 
-$count = 0;
+      $count = 0;
 
       $days1 = "";
       $st = date('Y-m-d',strtotime($final));
       $et = date('Y-m-d',strtotime($final1));
-
-      $st1    = new DateTime($st);
-      $et1    = new DateTime($et);
+      $dtz = new DateTimeZone('America/Chicago');
+      $st1    = new DateTime($st, $dtz);
+      $et1    = new DateTime($et, $dtz);
       $in1 = new DateInterval('P1D'); // 1 day interval
       $per1   = new DatePeriod($st1, $in1, $et1);
       foreach ($per1 as $day) 
@@ -322,34 +322,34 @@ $canbe = explode(',', $r21['can_be_usedid']);
 //echo "block1";
       ?>
 
-           <div class="col-md-6 col-sm-6 col-xs-12 pd-lr-4">
-           <a href="demo-venue.php?placeid=<?php echo $r21['place_id']; ?>&checkin=<?php echo $st; ?>&checkout=<?php echo $et; ?>">
-          <div class="border-box">
-          <div id="myCarousel<?php echo $r21['place_id']; ?>" class="carousel slide" data-ride="carousel">
+            <div class="col-md-6 col-sm-6 col-xs-12 pd-lr-4">
+              <a href="demo-venue.php?placeid=<?php echo $r21['place_id']; ?>&checkin=<?php echo $st; ?>&checkout=<?php echo $et; ?>">
+                <div class="border-box">
+                  <div id="myCarousel<?php echo $r21['place_id']; ?>" class="carousel slide" data-ride="carousel">
 
             <!-- Wrapper for slides -->
-            <div class="carousel-inner inner1" role="listbox">
+                    <div class="carousel-inner inner1" role="listbox">
               <?php foreach($images as $photos) {
                 if($photos=="")
               { continue;  } ?>
-              <div class="item">
-                <img src="images/placephotos/<?php echo $photos; ?>">
-              </div>
+                      <div class="item">
+                        <img src="images/placephotos/<?php echo $photos; ?>">
+                      </div>
               <?php } ?>
 
-            </div>
+                    </div>
 <?php if(count($images)>1){?>
             <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel<?php echo $r21['place_id']; ?>" role="button" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel<?php echo $r21['place_id']; ?>" role="button" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
+                    <a class="left carousel-control" href="#myCarousel<?php echo $r21['place_id']; ?>" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel<?php echo $r21['place_id']; ?>" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
             <?php }?>
-          </div>
+                  </div>
 <?php
 /*if(empty($r21['p_p_h']) && empty($r21['p_p_n']) && empty($r21['w_p_p_n'])){
 $que = mysqli_query($connect,"select * from calenderdata where placeid=".$r21['place_id']." group by placeid");
@@ -363,7 +363,7 @@ $que = mysqli_query($connect,"select * from calenderdata where placeid=".$r21['p
       }//if que numrows
           }*///calenderdata table prices ?>
 <?php $spacename=$r21['space_name'];?>
-          <h4 class="hotel-name"><?php 
+                  <h4 class="hotel-name"><?php 
           if(strlen($spacename)>10){echo substr($spacename,0,10)."..." ;}else{echo $spacename;} ?></h4>
 <!--
           <div class="req-cus rat-star">
@@ -387,9 +387,11 @@ $country = $r21['p_country'] ;
  <?php       break; case "India": $currency= "&#8377;"?>
 <?php }  ?>      
         
-          <div class="req-cus"><i class="fa fa-map-marker icncolor"></i> <?php $paddress=$r21['p_address'];
-          if(strlen($paddress)>40){echo substr($paddress,0,30)."..." ;}else{echo $paddress;} ?></div>
-          <div class="col-md-6 col-sm-6 col-xs-12 no-pad req-cus"><?php 
+                <div class="req-cus"><i class="fa fa-map-marker icncolor"></i> 
+                <?php $paddress=$r21['p_address'];
+          if(strlen($paddress)>40){echo substr($paddress,0,30)."..." ;}else{echo $paddress;} ?>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12 no-pad req-cus"><?php 
           if(!empty($r21['p_p_h']))
           {
               echo $currency;
@@ -410,12 +412,14 @@ $country = $r21['p_country'] ;
             echo ''.$r21['w_p_p_n'].' Per Week';
           }else{ echo "<br>";//'<i class="fa fa-inr icncolor"></i> ' .$sppw.' Per  Week<br>';
         }
-          ?> </div>
-          <div class="col-md-6 col-sm-6 col-xs-12 no-pad cap-txt req-cus"><i class="fa fa-users icncolor"></i>  Capacity: <?php echo $r21['capacity']; ?></div>
-          <div class="clearfix"></div>
-          </div>
-            </a>
-          </div>
+          ?> 
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 no-pad cap-txt req-cus"><i class="fa fa-users icncolor"></i>  Capacity: <?php echo $r21['capacity']; ?>
+              </div>
+              <div class="clearfix"></div>
+              </div>
+              </a>
+            </div>
 
       <?php
 
@@ -426,17 +430,17 @@ $country = $r21['p_country'] ;
                           }
      ?>
 
-<!--<input type="hidden" name="marker_addresses" id="marker_addresses" value="<?php echo rtrim($marker_addresses,">>>");?>" placeholder="">-->
-<!--<input type="hidden" name="marker_location" id="marker_location" value="<?php echo rtrim($marker_location,">>>");?>" placeholder="">-->
-</div>
+<input type="hidden" name="marker_addresses" id="marker_addresses" value="<?php echo rtrim($marker_addresses,">>>");?>" placeholder="">
+<input type="hidden" name="marker_location" id="marker_location" value="<?php echo rtrim($marker_location,">>>");?>" placeholder="">
+          </div>
 
 	<?php include 'lib/footer.php';?>
   <script src="js/forms-map.js"></script>
-</div>
-<div class="clearfix"></div>
-</div>
-</div><!--main-center-data close-->
-</div><!--row close-->
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div><!--main-center-data close-->
+  </div><!--row close-->
 	
 <!--======footer close======-->
 
