44628e41630ad1ca0bcc14e9e8badb0fd6102e77
Andy Guschwan <aguschwan@gmail.com>
2016-12-10 10:22:41 -0600

    calendar changes

--- cal_load.php
+++ cal_load.php
@@ -1,13 +1,16 @@
 <?php
 require_once 'connect.php';
-   
+
+$received = json_decode(file_get_contents('php://input'));
+$placeid = $received;
+error_log($received);  
 //$result = $db->query('SELECT * FROM events');
-$result=mysqli_query($connect,'SELECT * FROM calenderdata where placeid=1');
+$result=mysqli_query($connect,'SELECT * FROM calenderdata where placeid="'.$placeid.'"');
 
 class Event {}
 
 $events = array();
-//error_log($result);
+error_log($result);
 //foreach($result as $row) {
 while ($row = $result->fetch_array()) {
   //error_log($row['label']);
