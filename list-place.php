ac5b31f604dbe87e337370e60c4b4ed2b3876518
Andy Guschwan <aguschwan@gmail.com>
2016-12-14 15:26:21 -0600

    general fixes

--- list-place.php
+++ list-place.php
@@ -31,6 +31,24 @@ include_once('connect.php');?>
 
 <body>
 
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1446252165384800',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
 <div class="container-fluid"><!--container-fluid start-->
     <div class="row">
 
