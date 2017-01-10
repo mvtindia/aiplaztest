2521573c9a07c6471d2995759d2f1acbe5aa1b60
Andy Guschwan <aguschwan@gmail.com>
2017-01-10 15:39:02 -0600

    release c

--- demo-venue2.php
+++ demo-venue2.php
@@ -42,7 +42,7 @@ $placeid=$_REQUEST['placeid'];
 $date1=$_REQUEST['checkin'] . " 00:00";
 $date2=$_REQUEST['checkout'] . " 23:59";
 
-$query=mysqli_query($connect,'Select * from calenderdata inner join place on calenderdata.placeid = place.place_id where calenderdata.placeid="'.$placeid.'" and calenderdata.date1 <="'.$date1.'" and calenderdata.date2 > "'.$date1.'"');
+$query=mysqli_query($connect,'Select * from calenderdata inner join place on calenderdata.placeid = place.place_id where calenderdata.placeid="'.$placeid.'"');
 
 //if rows place table
 if($match=mysqli_fetch_array($query))
@@ -558,7 +558,7 @@ if($match1=mysqli_fetch_array($query1)){
 
 <div class="input-group mg-top20"> 
     <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-	<input type="text" id="hourdatepicker" name="checkin" value="<?php echo date_format(date_create($match['date1']), 'm/d/Y') ?>" placeholder="CheckIn" class="form-control bord-0">
+	<input type="text" id="hourdatepicker" name="checkin" value="<?php echo date_format(date_create($_REQUEST['checkin']), 'm/d/Y') ?>" placeholder="CheckIn" class="form-control bord-0">
 </div>
 
 </div>
