a61d2c915b08d9a87daf096d9ef40062b261cb77
Andy Guschwan <aguschwan@gmail.com>
2017-01-10 15:09:52 -0600

    release c

--- demo-venue2.php
+++ demo-venue2.php
@@ -42,7 +42,7 @@ $placeid=$_REQUEST['placeid'];
 $date1=$_REQUEST['checkin'] . " 00:00";
 $date2=$_REQUEST['checkout'] . " 23:59";
 
-$query=mysqli_query($connect,'Select * from calenderdata inner join place on calenderdata.placeid = place.place_id where calenderdata.placeid="'.$placeid.'" and calenderdata.date1 >="'.$date1.'" and calenderdata.date2 <= "'.$date2.'"');
+$query=mysqli_query($connect,'Select * from calenderdata inner join place on calenderdata.placeid = place.place_id where calenderdata.placeid="'.$placeid.'" and calenderdata.date1 >="'.$date1.'" and calenderdata.date2 > "'.$date1.'"');
 
 //if rows place table
 if($match=mysqli_fetch_array($query))
