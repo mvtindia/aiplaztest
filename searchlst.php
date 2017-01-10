8a177a7a3992471fd34266a4b383483493ce5fe7
Andy Guschwan <aguschwan@gmail.com>
2017-01-10 12:08:47 -0600

    release c

--- searchlst.php
+++ searchlst.php
@@ -1,10 +1,10 @@
 <?php include_once('connect.php');
 //if(isset($_REQUEST['searching'])) { 
   if (isset($_POST['chin'])) {
-    $chinvar = date_format(date_create($_POST['chin']), 'Y-m-d');
+    $chinvar = date_format(date_create($_POST['chin']), 'Y-m-d 00:00:00');
   }
   if (isset($_POST['chout'])) {
-    $choutvar = date_format(date_create($_POST['chout']), 'Y-m-d');
+    $choutvar = date_format(date_create($_POST['chout']), 'Y-m-d 23:59:00');
   }
   if(!empty($_REQUEST['place_loc'])){
     $place_loc = $_REQUEST['place_loc'];
@@ -65,12 +65,16 @@
   } else {
     $maxbud = 50000;
   }
-  $chinvarpl = date('Y-m-d', strtotime($chinvar . "-1 days"));
-  $choutvarpl = date('Y-m-d', strtotime($choutvar . "+1 days"));
+  $chinvarpl = date('Y-m-d HH:mm', strtotime($chinvar . "-1 days"));
+  $choutvarpl = date('Y-m-d HH:mm', strtotime($choutvar . "+1 days"));
   error_log($chinvar);
-  error_log($chinvarpl);
+  error_log($choutvar);
 //echo "select * from place where p_address like '%".$place_loc."%' and capacity >= '".$guests."' and ((p_p_h between ".$minbud." and ".$maxbud.") or (p_p_n between ".$minbud." and ".$maxbud.") or (w_p_p_n between ".$minbud." and ".$maxbud."))";
- $q21 = mysqli_query($connect,"select distinct a.place_id, a.p_address, a.p_city, a.p_state, a.p_country, a.postal_code, a.photo, a.space_name, a.capacity, a.p_p_h, a.p_p_n from place a, calenderdata b where place_id = placeid and user_id != '".$_SESSION['u_id']."' and status = 'Available' and p_address like '%".$place_loc."%' and capacity >= '".$guests."' and b.date1 >= '".$chinvarpl."' and b.date2 <= '".$choutvarpl."' and ((b.p_p_h between ".$minbud." and ".$maxbud.") or (b.p_p_n between ".$minbud." and ".$maxbud.")) order by a.place_id"); 
+ $q21 = mysqli_query($connect,"select distinct a.place_id, a.p_address, a.p_city, a.p_state, a.p_country, a.postal_code, 
+ a.photo, a.space_name, a.capacity, a.p_p_h, a.p_p_n from place a, calenderdata b where place_id = placeid and 
+ user_id != '".$_SESSION['u_id']."' and status = 'Available' and p_address like '%".$place_loc."%' and 
+ capacity >= '".$guests."' and ((b.date1 >= '".$chinvar."' and b.date2 <= '".$choutvar."') || (b.date1 < '".$choutvar."' and b.date2 >= '".$choutvar."') || (b.date1 <= '".$chinvar."' and b.date2 > '".$chinvar."'))   and 
+ ((b.p_p_h between ".$minbud." and ".$maxbud.") or (b.p_p_n between ".$minbud." and ".$maxbud.")) order by a.place_id"); 
   //$q21 = mysqli_query($connect,"select * from place where p_address like '%".$place_loc."%' "); 
 ?>
 <!doctype html>
