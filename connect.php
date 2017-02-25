985303d8a63bc1fda637b0b9da34fad5f1b6b502
Andy Guschwan <aguschwan@gmail.com>
2017-02-25 10:20:29 -0600

    production move

--- connect.php
+++ connect.php
@@ -3,9 +3,9 @@ error_reporting(0);
 
 
 //$connect=mysqli_connect("us-cdbr-azure-east-c.cloudapp.net","bcdc8eec7d9c69","70936a9a","TurtleRiver_testDB");
-//$connect=mysqli_connect("us-cdbr-azure-northcentral-b.cloudapp.net","b5ed6b723b4e0c","7bf07005","chicago_river");
+$connect=mysqli_connect("us-cdbr-azure-northcentral-b.cloudapp.net","b5ed6b723b4e0c","7bf07005","chicago_river");
 //"ap-cdbr-azure-east-c.cloudapp.net","b660bd5f89b637","ed5c4a00","Yamuna"
-$connect=mysqli_connect("localhost","root","cubs","andytest");
+//$connect=mysqli_connect("localhost","root","cubs","andytest");
 
 if($connect){
 echo "";
